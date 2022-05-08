54bbfb8f3f8d17f4f5ee23c86058b68cdb41785a
sutangzhi <1362217183@qq.com>
2022-05-08 19:38:00 +0800

    我第二次在idea中提交代码到本地库

--- src/main/java/com/git/pkg/TestGit.java
+++ src/main/java/com/git/pkg/TestGit.java
@@ -3,5 +3,6 @@ package com.git.pkg;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("hello git!!");
+        System.out.println("hello git 2 !!");
     }
 }
